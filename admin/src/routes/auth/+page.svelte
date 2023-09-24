7857485fc84fcd8cc65b8ef5c766af9202d1aa27
TheWisePigeon <zozozozeph@gmail.com>
2023-09-24 14:30:15 +0000

    fix: ui: fix error message display

--- admin/src/routes/auth/+page.svelte
+++ admin/src/routes/auth/+page.svelte
@@ -3,7 +3,8 @@
 
 	let username = '';
 	let password = '';
-	let err_message = '';
+	let err_message =
+		'Something unexpected happened. Open an issue on github if the problem persists';
 	let err = false;
 	let loading = false;
 	let api_url = 'http://127.0.0.1:8080';
@@ -44,34 +45,36 @@
 </script>
 
 <div class=" flex flex-col items-center justify-center h-screen gap-5">
-	<h1 class="text-md">Log into RestDis</h1>
-	<form class="flex flex-col gap-5" on:submit|preventDefault={login}>
-		<input
-			required
-			bind:value={username}
-			class="p-2 rounded-md w-72 border"
-			type="text"
-			placeholder="Username"
-		/>
-		<input
-			required
-			bind:value={password}
-			class="p-2 rounded-md w-72 border"
-			type="password"
-			placeholder="Password"
-		/>
-		<button
-			disabled={loading}
-			class={`${loading ? 'bg-blue-500' : ' bg-blue-600'} p-2 rounded-md text-white`}
-		>
-			{#if loading}
-				<h1>...</h1>
-			{:else}
-				<h1>Login</h1>
+	<div class=" flex flex-col items-center justify-center w-96 ">
+		<h1 class="text-md pb-5">Log into RestDis</h1>
+		<form class="flex flex-col gap-5 w-full" on:submit|preventDefault={login}>
+			<input
+				required
+				bind:value={username}
+				class="p-2 rounded-md w-72 border w-full"
+				type="text"
+				placeholder="Username"
+			/>
+			<input
+				required
+				bind:value={password}
+				class="p-2 rounded-md w-72 border w-full"
+				type="password"
+				placeholder="Password"
+			/>
+			<button
+				disabled={loading}
+				class={`${loading ? 'bg-blue-500' : ' bg-blue-600'} p-2 rounded-md text-white`}
+			>
+				{#if loading}
+					<h1>...</h1>
+				{:else}
+					<h1>Login</h1>
+				{/if}
+			</button>
+			{#if err}
+				<h1 class="text-center text-sm text-red-500 w-full">{err_message}</h1>
 			{/if}
-		</button>
-		{#if err}
-			<h1 class="text-center text-sm text-red-500 w-full line-break">{err_message}</h1>
-		{/if}
-	</form>
+		</form>
+	</div>
 </div>
