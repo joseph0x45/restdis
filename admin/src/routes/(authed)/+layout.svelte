91e8ed7ddb6d02bcfbde886c00e6d4a516fbfc9b
TheWisePigeon <zozozozeph@gmail.com>
2023-09-25 10:30:42 +0000

    ui: add basic admin panel layout

---
+++ admin/src/routes/(authed)/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<header class="p-2 w-full border-b">
+	<div class="flex justify-between items-center w-full">
+		<h1 class="text-xl">Restdis v0.1</h1>
+		<h1>Logged in as username</h1>
+	</div>
+</header>
+<div class=" flex w-full gap-5 p-2 border-b">
+<a class=" hover:underline" href="/console/">Profile</a>
+	<a class=" hover:underline" href="/console/">Access tokens</a>
+	<a class=" hover:underline" href="/console/">Users</a>
+	<a class=" hover:underline" href="/console/">Configuration</a>
+</div>
+<slot />
