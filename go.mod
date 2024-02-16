25908da5e0f989286a5ffab46ac65dca7e88bdeb
TheWisePigeon <zozozozeph@gmail.com>
2024-02-16 20:21:34 +0000

    load env vars before launching server

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module restdis
 
 go 1.22.0
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"log"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
 	log.Println("Hello nigga")
 }
