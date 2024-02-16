1e1cab8c10d7230df28acf52109929508501570e
TheWisePigeon <zozozozeph@gmail.com>
2024-02-16 20:21:16 +0000

    update db schema

---
+++ schema.sql
@@ -0,0 +1,15 @@
+create table users (
+  id integer primary key autoincrement not null,
+  username text not null unique,
+  password text not null,
+  can_manage_users bool not null default false,
+  can_manage_tokens bool not null default false,
+  active bool not null default true
+);
+
+create table access_keys (
+);
+
+create table logs (
+
+);
